b3b4daa0226ae7e594095732fdd9bbe338aa5762
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2018-10-03 09:17:32 +0200

    Har implementeret metoder

--- CalculatorLibrary/Calculator.cs
+++ CalculatorLibrary/Calculator.cs
@@ -8,5 +8,50 @@ namespace CalculatorLibrary
 {
     public class Calculator
     {
+        public static double Add(double v1, double v2)
+        {
+            double result = v1 + v2;
+            return result;
+        }
+
+        public static double Subtract(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Multiply(double v1, double v2)
+        {
+            return 0;
+        }
+
+        public static double Divide(double v1, double v2)
+        {
+            return 0;
+        }
+
+        public static double Sum(double v1, double v2)
+        {
+            return 0;
+        }
+
+        public static double Sum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Minimum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Maximum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Average(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
